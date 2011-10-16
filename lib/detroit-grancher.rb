3a34c4ac815052a3c6c643a587f67cb929a33178
7rans <transfire@gmail.com>
2011-10-16 10:28:04 -0400

    Add station methods.

--- lib/detroit-grancher.rb
+++ lib/detroit-grancher.rb
@@ -1,22 +1,22 @@
 module Detroit
 
-  #
+  # Convenicene method for creating a new Grancher tool instance.
   def Grancher(options={})
     Grancher.new(options)
   end
 
+  # IMPORTANT! Grancher Tool is being deprecated in favor of the new GitHub
+  # tool. Grancher has issues with Ruby 1.9 (due to encoding problems
+  # in Gash library) and show no signs of being fixes.
+  #
+  # IMPORTANT! This toll only works with Ruby 1.8.x.
+  #
   # This tool copies designated files to a git branch.
   # This is useful for dealing with situations like GitHub's
   # gh-pages branch for hosting project websites.[1]
   #
   # [1] A poor design copied from the Git project itself.
   class Grancher < Tool
-
-    ## Gancher will be available automatically if the POM repository
-    ## entry indicates the use of GitHub.
-    #autorun do |project|
-    #  /github.com/ =~ project.metadata.repository
-    #end
 
     # The brach into which to save the files.
     attr_accessor :branch
@@ -27,21 +27,23 @@ module Detroit
     # The repository loaiton (defaults to current project directory).
     #attr_accessor :repo
 
-    # Message to output.
+    # Commit message.
     #attr_accessor :message
 
-    # List of any files/directory to not overwrite in branch.
+    # List of any files/directories to not remove from the branch.
     attr_accessor :keep
 
-    # Do not overwrite anything. Defaults to +trial+ setting.
+    # Do not remove any files from the branh.
     attr_accessor :keep_all
 
     # List of directories and files to transfer.
     # If a single directory entry is given then the contents
-    # of that directory will be transfered.
+    # of that directory will be transfered. Otherwise this 
+    # can be an associative array or hash mapping seource to
+    # destination.
     attr_reader :sitemap
 
-    #
+    # Set sitemap.
     def sitemap=(entries)
       case entries
       when String, Symbol
@@ -51,6 +53,23 @@ module Detroit
       end
     end
 
+
+    #  A S S E M B L Y  S T A T I O N S
+
+    # Attach pre-publish method to pre-publish assembly station.
+    def station_pre_publish
+      transfer
+    end
+
+    # Attach publish method to publish assembly station.
+    def station_publish
+      publish
+    end
+
+
+    #  S E R V I C E  M E T H O D S
+
+    # Cached Grancter instance.
     def grancher
       @grancher ||= ::Grancher.new do |g|
         g.branch  = branch
@@ -75,7 +94,7 @@ module Detroit
       end
     end
 
-    #
+    # Commit file to branch.
     def transfer
       sleep 1  # FIXME: had to pause so grancher will not bomb!
       require 'grancher'
@@ -83,7 +102,10 @@ module Detroit
       report "Tranferred site files to #{branch}."
     end
 
-    #
+    # Same as transfer.
+    alias_method :pre_publish, :transfer
+
+    # Push files to remote.
     def publish
       require 'grancher'
       grancher.push
@@ -100,35 +122,26 @@ module Detroit
       #@keep_all ||= trial?
     end
 
-    # Default sitemap includes the website directoy, if it exists
-    # and doc if it exists. Eg.
+    # Default sitemap includes the website directoy, if it exists.
+    # Otherwise it looks for a `doc` or `docs` directory.
     #
-    #    - site
-    #    - doc
-    #
-    # Otherwise it includes just the doc/rdoc or doc directory.
-    #
-    #--
-    # We have loop over the contents of the site directory in order
-    # to pick up symlinks b/c Grancher doesn't support them.
-    #++
+    # NOTE: We have loop over the contents of the site directory
+    # in order to pick up symlinks b/c Grancher doesn't support them.
     def default_sitemap
-      sm = []
-      site = Dir['{site,web,website,www}'].first
-      if site
-        #sm << site
-        paths = Dir.entries(site)
+      sm  = []
+      if dir = Dir['{site,web,website,www}'].first
+        #sm << dir
+        paths = Dir.entries(dir)
         paths.each do |path|
           next if path == '.' or path == '..'
-          sm << [File.join(site, path), path]
+          sm << [File.join(dir, path), path]
         end
-      else
-        if path = Dir["#{name}/doc/rdoc"].first
-          sm << path
-        elsif path = Dir['doc/rdoc'].first
-          sm << path
-        elsif path = Dir['doc'].first
-          sm << path 
+      elsif dir = Dir["{doc,docs}"].first
+        #sm << dir
+        paths = Dir.entries(dir)
+        paths.each do |path|
+          next if path == '.' or path == '..'
+          sm << [File.join(dir, path), path]
         end
       end
       sm
